02effa6737fdf546c3723f91b66b007efc34b606
Dimitri Couwenberg <couwenberg@gmail.com>
2017-06-29 21:49:35 +1000

    listing pokemons

--- app/components/Pokemon.jsx
+++ app/components/Pokemon.jsx
@@ -1,12 +1,52 @@
 import React, {Component} from 'react';
 
-
 export default class Pokemon extends Component {
 
+
+    getId(pk) {
+        const ar = pk.url.split("/");
+        let nr = ar[ar.length - 2];
+        return nr;
+    }
+
+    getImageFilename(pk) {
+        let filename ="../app/images/" + this.getId(pk) + ".png";
+        return filename;
+    }
+
     render() {
-     
-        return (<div>
-          
+        let display;
+
+        display = this.props.data.map
+        (
+            (poke) =>
+                (
+                    <div>
+                        <div key={poke.name} className="col-lg-3 col-sm-6 hidden-xs">
+                            <img src={this.getImageFilename(poke)} alt=""/>
+                            <p>#{this.getId(poke)}</p>
+                            <p> {poke.name}</p>
+
+                        </div>
+
+                        <div className="row hidden-lg hidden-sm hidden-md">
+                            <div className="col-xs-6">
+                                <img className="big-box" src={this.getImageFilename(poke)} alt=""/>
+                            </div>
+                            <div className="col-xs-6">
+                                <div className="mini-box">
+                                    <div className="col-xs-12">#{this.getId(poke)}</div>
+                                    <div className="col-xs-12">{poke.name}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )
+        )
+
+        return (
+            <div>
+                {display}
             </div>
         );
     }
